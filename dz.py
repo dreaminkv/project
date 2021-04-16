30eeebbd26ef0863cbbc38f30de6737b27364ad6
dreaminkv <79664931+dreaminkv@users.noreply.github.com>
2021-04-16 14:40:45 +0300

    Update dz.py

--- dz.py
+++ dz.py
@@ -7,3 +7,17 @@ for el in some_list1:
         else:
             some_list2.append(el)
 print(' '.join(some_list2))
+
+
+some_list1 = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+
+for i in range(len(some_list1)):
+    el = some_list1.pop(i-i)
+    if el[-1].isdigit() and int(el) < 10:
+        el = '"' + el[:-1] + '0' + el[-1] + '"'
+        some_list1.append(el)
+    else:
+        some_list1.append(el)
+        
+        
+print(' '.join(some_list1))
