c6642f89585a73d07ee18d1f0897172e5cf5dad4
dreaminkv <79664931+dreaminkv@users.noreply.github.com>
2021-04-16 14:00:03 +0300

    Create dz.py

---
+++ dz.py
@@ -0,0 +1,9 @@
+some_list1 = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+some_list2 =[]
+for el in some_list1:
+        if el[-1].isdigit() and int(el) < 10:
+            el = '"' + el[:-1] + '0' + el[-1] + '"'
+            some_list2.append(el)
+        else:
+            some_list2.append(el)
+print(' '.join(some_list2))
